e5bfbb90285c2a6ebc81d01677d26cc4c272dc8f
Maralena86 <mari16cobain@msn.com>
2023-07-26 17:21:53 +0200

    test git change

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 echo "hola";
-//Fet the File
+//Get the File
 $nameFile = "stations.csv";
 $myFile = fopen($nameFile, "r");
 
@@ -8,6 +8,7 @@ if (!($nameFile)){
     print("It's not possible to open the file");
 }
 else{
+    //Show the specific information from the csv file
     while( $line = fgetcsv($myFile, null, ";")){
         print( $line[2]." ".$line[3]. " ".$line[4]." ". $line[5])."<br>";
     }
